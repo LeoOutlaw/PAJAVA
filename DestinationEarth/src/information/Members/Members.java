7d2f57c9cc40eb098df67fa4cf338058531ab4d8
Tiago1996 <49624069+Tiago1996@users.noreply.github.com>
2019-04-15 18:22:52 +0100

    v1

---
+++ DestinationEarth/src/information/Members/Members.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package information.Members;
+
+/**
+ *
+ * @author Leonardo
+ */
+public abstract class Members {
+    String name;
+    int attack;
+    int movement;
+    
+    public abstract String getName();
+    public abstract int getMovement();
+    public abstract void setMovement(int movement);
+}
