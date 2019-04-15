aa9079a4b1b633a3c1770aa4c0825506765ab691
LeoOutlaw <leorr43@gmail.com>
2019-04-15 18:13:58 +0100

    Revert "Doctor"
    This reverts commit 0fd3b438462782962eebe4ce4bd3dbc24b07cf7a.

--- DestinationEarth/src/information/Members/Members.java
+++ DestinationEarth/src/information/Members/Members.java
@@ -15,6 +15,7 @@ public abstract class Members {
     int movement;
     
     public abstract String getName();
+    public abstract void setName(String name);
     public abstract int getMovement();
-    public abstract void setMovement(int movement);
+    public abstract void setMovement();
 }
